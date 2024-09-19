b8981a02c9b8700fd94e2d9a74888ef90230fc36
Farid Domat <fariddomat.000@gmail.com>
2024-09-19 19:10:14 +0300

    fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = ({ isLoggedIn, onLogout }) => {
   return (
@@ -17,20 +17,34 @@ const Navbar = ({ isLoggedIn, onLogout }) => {
 
       <div className="branding d-flex align-items-center">
         <div className="container position-relative d-flex align-items-center justify-content-between">
-          <Link to="/" className="logo d-flex align-items-center">
+          <NavLink to="/" className="logo d-flex align-items-center">
             <h1 className="sitename">Your Website</h1>
-          </Link>
+          </NavLink>
           <nav className="navmenu">
             <ul>
-              <li><Link to="/" className="active">Home</Link></li>
-              <li><Link to="/companies">Companies</Link></li>
-              <li><Link to="/events">Events</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/contact-us">Contact Us</Link></li>
+              <li>
+                <NavLink to="/" activeClassName="active">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/companies" activeClassName="active">Companies</NavLink>
+              </li>
+              <li>
+                <NavLink to="/events" activeClassName="active">Events</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" activeClassName="active">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact-us" activeClassName="active">Contact Us</NavLink>
+              </li>
               {isLoggedIn ? (
-                <li><Link to="/" onClick={onLogout}>Logout</Link></li>
+                <li>
+                  <NavLink to="/login" onClick={onLogout}>Logout</NavLink>
+                </li>
               ) : (
-                <li><Link to="/login">Login</Link></li>
+                <li>
+                  <NavLink to="/login" activeClassName="active">Login</NavLink>
+                </li>
               )}
             </ul>
             <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
